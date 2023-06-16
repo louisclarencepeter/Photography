28a370010867bbba59bdc6aeb575206241f773ef
Louis Peter <louisclarencepeters@gmail.com>
2023-06-16 11:50:51 +0200

    added some animation on main page

---
+++ script.js
@@ -0,0 +1,15 @@
+/* window.onload = function() {
+    // This code will execute when the website finishes loading
+    
+    // Find the image element
+    const image = document.getElementsByClassName("item");
+    
+    // Add a class to trigger the animation
+    image.classList.add("animate");
+    
+    // Scale the image back to its original size after it has loaded
+    image.onload = function() {
+      image.style.transform = "scale(1)";
+    };
+  };
+   */
