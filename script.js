5712aeb22e7533da97d6a2561a8c53fca9490a6c
Louis Peter <louisclarencepeters@gmail.com>
2023-09-14 12:00:29 +0200

    added the code for the burger menu

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ window.addEventListener("scroll", reveal);
 
 // To check the scroll position on page load
 reveal();
+
+// Get a reference to the checkbox element
+const checkbox = document.querySelector('.checkbox');
+
+// Get a reference to the menu items container
+const menuItems = document.querySelector('.menu-items');
+
+// Add an event listener to the checkbox to toggle the 'active' class
+checkbox.addEventListener('change', function() {
+  if (this.checked) {
+    // Checkbox is checked, add the 'active' class to menu items
+    menuItems.classList.add('active');
+  } else {
+    // Checkbox is unchecked, remove the 'active' class from menu items
+    menuItems.classList.remove('active');
+  }
+});
